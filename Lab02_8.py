40a010bb9ae5fdd1dfd2b8e0ab7783e723c3460f
unknown <08250269.sce@rub.edu.bt>
2026-04-28 11:34:40 +0530

    my part

--- Lab02_8.py
+++ Lab02_8.py
@@ -34,7 +34,69 @@ print("\nList of Students:")
 for key, value in students.items():
     print(f"{key}. {value}")    
 
-   
+print("-----------------------------------------------------------------------------")
+print("QUIZ")
+# For each student,they have three questions based on the unique value and calculate their score
+for name in students.values():
+
+    print("Student:", name)
+
+    # check if empty name
+    if name == "": # Blank name entered
+        print("Empty name. Skipping...") 
+    else:
+        score = 0 # Initialize score for each student
+
+        # Q1 
+        a = int(input("Q1: " + str(last_two_id) + " + 2 = "))
+        if a == last_two_id + 2:
+            score = score + 1
+
+        # Q2
+        b = int(input("Q2: " + str(last_two_id) + " * 3 = "))
+        if b == last_two_id * 3:
+            score = score + 1
+
+        # Q3
+        c = int(input("Q3: " + str(last_two_id) + " + 5 = "))
+        if c == last_two_id + 5:
+            score = score + 1
+        print("-----------------------------------------------------------------------------")
+        print()
+        print("You have Scored:", score) # performance, certificate, warning, stars
+
+        # performance
+        if score == 3:
+            print("Your Performance: Excellent") 
+        elif score == 2:
+            print("Your Performance: Good")
+        elif score == 1:
+            print("Your Performance: Average")
+        else:
+            print("YourPerformance: Poor")
+
+        # certificate
+        if score >= 2:
+            print("Eligible for Certificate: Yes")
+            print()
+        else:
+            print("Eligible for Certificate: No")
+            print()
+
+        # warning
+        if score == 0: # Warning for score 0
+            print("Warning: Score is 0")
+            print()
+
+        # stars
+        print("Stars:")
+        if score == 0:
+            print(" ") # No stars for score 0
+        else: # Print stars in a pattern based on the score
+            i = 1
+            while i <= score: 
+                print("*" * i)
+                i = i + 1 # Increment i to print the next line of stars
 
 
 
